f723ad3ffc60dbc4428b56f0110c5127a67fe16b
Madery Romain <romain.madery92@gmail.com>
2023-06-14 16:59:11 +0200

    Add Test class

---
+++ src/test/java/com/example/projet_bibliotheque/BibliothequeTest.java
@@ -0,0 +1,14 @@
+package com.example.projet_bibliotheque;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class BibliothequeTest {
+    @Test
+    void loadXML() {
+    }
+
+    @Test
+    void toXml() {
+    }
+}
