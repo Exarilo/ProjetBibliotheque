ee79b6a6cda5fd809ee699d8e972a9572cb905f8
redhaaoudia <60343695+redhaaoudia@users.noreply.github.com>
2023-06-15 10:18:44 +0200

    Ajout de fonction

--- src/test/java/com/example/projet_bibliotheque/AuteurTest.java
+++ src/test/java/com/example/projet_bibliotheque/AuteurTest.java
@@ -8,17 +8,40 @@ class AuteurTest {
 
     @Test
     void getNom() {
+
+        Auteur auteur = new Auteur("Aoudia","Redha");
+
+        assertEquals("Aoudia",auteur.getNom());
     }
 
     @Test
     void setNom() {
+        Auteur auteur = new Auteur("Aoudia","Redha");
+
+        assertEquals("Aoudia",auteur.getNom());
+
+        auteur.setNom("Madery");
+
+        assertEquals("Madery",auteur.getNom());
+
     }
 
     @Test
     void getPrenom() {
+
+        Auteur auteur = new Auteur("Aoudia","Redha");
+
+        assertEquals("Redha",auteur.getPrenom());
     }
 
     @Test
     void setPrenom() {
+        Auteur auteur = new Auteur("Aoudia","Redha");
+
+        assertEquals("Redha",auteur.getPrenom());
+
+        auteur.setPrenom("Romain");
+
+        assertEquals("Romain",auteur.getPrenom());
     }
 }
