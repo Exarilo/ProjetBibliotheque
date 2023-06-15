7c9551aa8efa77ade394c7f4d36b294578353308
Madery Romain <romain.madery92@gmail.com>
2023-06-15 10:27:43 +0200

    Fix auteur class

--- src/main/java/com/example/projet_bibliotheque/Auteur.java
+++ src/main/java/com/example/projet_bibliotheque/Auteur.java
@@ -3,33 +3,28 @@ package com.example.projet_bibliotheque;
 import javafx.beans.property.SimpleStringProperty;
 
 public class Auteur {
-    private SimpleStringProperty nom = new SimpleStringProperty();
-    private SimpleStringProperty prenom = new SimpleStringProperty();
+    private String nom;
+    private String prenom;
 
     public Auteur(String nom, String prenom) {
+        this.nom=nom;
+        this.prenom=prenom;
     }
 
     public String getNom() {
-        return nom.get();
+        return this.nom;
     }
 
-    public SimpleStringProperty nomProperty() {
-        return nom;
-    }
 
-    public void setNom(String nom) {
-        this.nom.set(nom);
+    public void setNom(String nouveauNom) {
+        this.nom=nouveauNom;
     }
 
     public String getPrenom() {
-        return prenom.get();
-    }
-
-    public SimpleStringProperty prenomProperty() {
         return prenom;
     }
 
-    public void setPrenom(String prenom) {
-        this.prenom.set(prenom);
+    public void setPrenom(String nouveauPrenom) {
+        this.prenom=nouveauPrenom;
     }
 }
