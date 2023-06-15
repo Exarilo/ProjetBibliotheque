bedc1705b8edf4ad8d0b510ec29d794b2331fe85
Madery Romain <romain.madery92@gmail.com>
2023-06-15 10:05:46 +0200

    Update Units tests

--- src/test/java/com/example/projet_bibliotheque/LivreTest.java
+++ src/test/java/com/example/projet_bibliotheque/LivreTest.java
@@ -7,66 +7,117 @@ import static org.junit.jupiter.api.Assertions.*;
 class LivreTest {
 
     @Test
-    void getTitre() {
+    void testGetTitre() {
+        Livre livre = new Livre();
+        livre.setTitre("Titre du livre");
+        assertEquals("Titre du livre", livre.getTitre());
     }
 
     @Test
-    void setTitre() {
+    void testSetTitre() {
+        Livre livre = new Livre();
+        livre.setTitre("Titre du livre");
+        assertEquals("Titre du livre", livre.getTitre());
     }
 
     @Test
-    void getNomAuteur() {
+    void testGetNomAuteur() {
+        Livre livre = new Livre();
+        livre.setNomAuteur("Nom de l'auteur");
+        assertEquals("Nom de l'auteur", livre.getNomAuteur());
     }
 
     @Test
-    void setNomAuteur() {
+    void testSetNomAuteur() {
+        Livre livre = new Livre();
+        livre.setNomAuteur("Nom de l'auteur");
+        assertEquals("Nom de l'auteur", livre.getNomAuteur());
     }
 
     @Test
-    void getPrenomAuteur() {
+    void testGetPrenomAuteur() {
+        Livre livre = new Livre();
+        livre.setPrenomAuteur("Prénom de l'auteur");
+        assertEquals("Prénom de l'auteur", livre.getPrenomAuteur());
     }
 
     @Test
-    void setPrenomAuteur() {
+    void testSetPrenomAuteur() {
+        Livre livre = new Livre();
+        livre.setPrenomAuteur("Prénom de l'auteur");
+        assertEquals("Prénom de l'auteur", livre.getPrenomAuteur());
     }
 
     @Test
-    void getPresentation() {
+    void testGetPresentation() {
+        Livre livre = new Livre();
+        livre.setPresentation("Présentation du livre");
+        assertEquals("Présentation du livre", livre.getPresentation());
     }
 
     @Test
-    void setPresentation() {
+    void testSetPresentation() {
+        Livre livre = new Livre();
+        livre.setPresentation("Présentation du livre");
+        assertEquals("Présentation du livre", livre.getPresentation());
     }
 
     @Test
-    void getParution() {
+    void testGetParution() {
+        Livre livre = new Livre();
+        livre.setParution(2023);
+        assertEquals(2023, livre.getParution());
     }
 
     @Test
-    void setParution() {
+    void testSetParution() {
+        Livre livre = new Livre();
+        livre.setParution(2023);
+        assertEquals(2023, livre.getParution());
     }
 
     @Test
-    void getColonne() {
+    void testGetColonne() {
+        Livre livre = new Livre();
+        livre.setColonne(5);
+        assertEquals(5, livre.getColonne());
     }
 
     @Test
-    void setColonne() {
+    void testSetColonne() {
+        Livre livre = new Livre();
+        livre.setColonne(5);
+        assertEquals(5, livre.getColonne());
     }
 
     @Test
-    void getRangee() {
+    void testGetRangee() {
+        Livre livre = new Livre();
+        livre.setRangee(2);
+        assertEquals(2, livre.getRangee());
     }
 
     @Test
-    void setRangee() {
+    void testSetRangee() {
+        Livre livre = new Livre();
+        livre.setRangee(2);
+        assertEquals(2, livre.getRangee());
     }
 
     @Test
-    void getAuteur() {
+    void testGetAuteur() {
+        Livre livre = new Livre();
+        livre.setNomAuteur("Nom de l'auteur");
+        livre.setPrenomAuteur("Prénom de l'auteur");
+        assertEquals("Nom de l'auteur Prénom de l'auteur", livre.getAuteur());
     }
 
     @Test
-    void setAuteur() {
+    void testSetAuteur() {
+        Livre livre = new Livre();
+        Auteur auteur = new Auteur("NomAuteur","PrenomAuteur");
+        livre.setAuteur(auteur);
+        assertEquals("NomAuteur", livre.getNomAuteur());
+        assertEquals("PrenomAuteur", livre.getPrenomAuteur());
     }
 }
