ba417107e8ea8c168f65c6b1f8015e0749a154a2
Madery Romain <romain.madery92@gmail.com>
2023-06-23 12:44:30 +0200

    Add connexion page

---
+++ src/main/java/com/example/projet_bibliotheque/ConnexionController.java
@@ -0,0 +1,111 @@
+package com.example.projet_bibliotheque;
+
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import java.io.IOException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+
+public class ConnexionController implements Initializable  {
+    private Map<String, String> validUsers = new HashMap<String, String>();
+
+    @FXML private Label labelConnexionInscription;
+    @FXML private Label btMdpOublie;
+    @FXML private Button btInscription;
+    @FXML private TextField fieldUsername;
+    @FXML private PasswordField fieldPassword;
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        validUsers.put("admin", "admin");
+    }
+    @FXML
+    void handleInscription(ActionEvent event){
+        if(labelConnexionInscription.getText() =="Inscription")
+        {
+            labelConnexionInscription.setText("Connexion");
+            btMdpOublie.setVisible(true);
+            btInscription.setText("S'inscrire");
+        }
+        else
+        {
+            labelConnexionInscription.setText("Inscription");
+            btInscription.setText("Se connecter");
+            btMdpOublie.setVisible(false);
+        }
+    }
+
+
+    public void handleOublieMDP(javafx.scene.input.MouseEvent mouseEvent) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Information");
+        alert.setHeaderText(null);
+        alert.setContentText("Username : admin\nPassword : admin");
+        alert.showAndWait();
+    }
+
+    @FXML
+    void handleValider(ActionEvent event) throws IOException {
+        if(labelConnexionInscription.getText() =="Inscription")
+        {
+            if(fieldUsername.getText()==""||fieldPassword.getText()==""){
+                Alert alert = new Alert(Alert.AlertType.WARNING);
+                alert.setTitle("Warning");
+                alert.setHeaderText(null);
+                alert.setContentText("Vous devez saisir tout les champs");
+                alert.showAndWait();
+                return;
+            }
+            validUsers.put(fieldUsername.getText(), fieldPassword.getText());
+            labelConnexionInscription.setText("Connexion");
+            btMdpOublie.setVisible(true);
+            btInscription.setText("S'inscrire");
+            fieldUsername.setText("");
+            fieldPassword.setText("");
+        }
+        else
+        {
+            if(validUsers.get(fieldUsername.getText()).equals(fieldPassword.getText())){
+                Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                Stage newStage = new Stage();
+
+                FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main-view.fxml"));
+                Parent root = fxmlLoader.load();
+
+                Scene newScene = new Scene(root, 950, 600);
+
+                newStage.setTitle("Bibliotheque");
+                newStage.setResizable(false);
+                Image icon = new Image("https://cdn-icons-png.flaticon.com/512/5606/5606075.png");
+                newStage.getIcons().add(icon);
+                newStage.setScene(newScene);
+
+                newStage.show();
+                currentStage.close();
+            }
+            else{
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error");
+                alert.setHeaderText(null);
+                alert.setContentText("Mauvais username ou password");
+                alert.showAndWait();
+            }
+        }
+    }
+
+
+
+}
--- src/main/java/com/example/projet_bibliotheque/Main.java
+++ src/main/java/com/example/projet_bibliotheque/Main.java
@@ -17,6 +17,7 @@ public class Main extends Application {
      * La methode start est le point d'entré de l'application. elle vient construire l'IHM en se
      * basant sur main-view.fxml (construit avec Scene Builder)
      */
+    /*
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 950, 600);
@@ -27,7 +28,17 @@ public class Main extends Application {
         stage.setScene(scene);
         stage.show();
     }
-
+    */
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("connexion-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Bibliotheque");
+        stage.setResizable(false);
+        Image icon = new Image("https://cdn-icons-png.flaticon.com/512/5606/5606075.png");
+        stage.getIcons().add(icon);
+        stage.setScene(scene);
+        stage.show();
+    }
     public static void main(String[] args) {
         launch();
     }
